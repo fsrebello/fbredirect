f4aeebaf0c028a7e16c9869348e9d1fbe3b653e5
fsrebello <FSREBELLO@YAHOO.COM>
2024-08-20 17:56:06 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Image from 'next/Image';
+import Image from 'next/image';
 import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
